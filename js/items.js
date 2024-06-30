e514e39d8e4807365ef7ff8e67bfdc4524f8c6d7
Celio <77380157+cponcet07@users.noreply.github.com>
2024-06-30 12:56:00 +0200

    fichier de depart version 1

---
+++ js/items.js
@@ -0,0 +1,20 @@
+const items = [
+    {
+        "title": "Role SCP-049",
+        "price": 10,
+        "description": "Un rôle spécial pour incarner SCP-049, le docteur de la peste.",
+        "image": "test.jpg"
+    },
+    {
+        "title": "Accessoire: Lunettes de Vision Nocturne",
+        "price": 5,
+        "description": "Des lunettes spéciales pour voir dans le noir.",
+        "image": "test.jpg"
+    },
+    {
+        "title": "Role SCP-173",
+        "price": 15,
+        "description": "Un rôle spécial pour incarner SCP-173, la statue qui bouge.",
+        "image": "test.jpg"
+    }
+];
---
+++ js/modalMessage.js
@@ -0,0 +1,5 @@
+const modalMessage = `
+    <p>Bienvenue dans notre boutique SCP. Ici, vous pouvez acheter des rôles spéciaux et des accessoires pour améliorer votre expérience de jeu. Cliquez sur "Voir plus" pour obtenir plus de détails sur chaque article et pour procéder à l'achat.</p>
+    <p>Pour acheter un article, cliquez sur le bouton "Acheter" dans la fenêtre de détails. Vous serez redirigé vers PayPal pour finaliser votre achat.</p>
+    <p>Amusez-vous bien et faites attention aux SCP !</p>
+`;
