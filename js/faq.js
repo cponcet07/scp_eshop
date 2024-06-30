089bbcc567f8dd84f23c529c4d663716f3027087
Celio <77380157+cponcet07@users.noreply.github.com>
2024-06-30 19:40:55 +0200

    ajout de la faq et l'acceuil + changemet index

---
+++ js/faq.js
@@ -0,0 +1,14 @@
+const faqs = [
+    {
+        "question": "Quel est le rôle de SCP-049?",
+        "answer": "SCP-049 est connu comme le docteur de la peste, un être humanoïde avec des capacités mystérieuses."
+    },
+    {
+        "question": "Comment obtenir des accessoires?",
+        "answer": "Vous pouvez acheter des accessoires dans notre boutique en ligne ou les gagner en participant à des événements."
+    },
+    {
+        "question": "Qu'est-ce que SCP-173?",
+        "answer": "SCP-173 est une statue qui bouge uniquement quand elle n'est pas observée. Elle est très dangereuse."
+    }
+];
